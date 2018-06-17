1dc8a0d2c7e21308b028fe6db8d320d9171ae9e5
Atsushi Yoshinaga <ayoshinaga001@nam.ad.pwcinternal.com>
2018-06-17 12:41:12 +0900

    "initialize git repository"

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Summary (ファイルヘッダーの場合はピリオドなし)
+ *
+ * Description.
+ * BootStrap4を利用したThemeを作成する
+ * Home.php がない場合に、index.php が自動で読み込まれるテンプレート階層という仕組みがある
+ * @package WordPress
+ * @since 0.0.1
+ */
+?>
+<?php get_header(); ?>
+    <div class="alert alert-success" role="alert">Bootstrap4を利用する準備OK!!</div>
+<?php get_footer(); ?>
